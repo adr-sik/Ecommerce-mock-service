cf3b644168c993233e672a3ab28461bcfb3a5059
adr-sik <adrsik1997@gmail.com>
2025-11-17 20:11:45 +0100

    Changed OrderProduct to contain price during order time

--- Server/Models/OrderProduct.cs
+++ Server/Models/OrderProduct.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace Server.Models
     {
         public int OrderId { get; set; }
         public int ProductId { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal UnitPrice { get; set; }
         public int Amount { get; set; }
     }
 }
